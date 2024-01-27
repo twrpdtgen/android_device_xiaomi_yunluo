f203793ab952d6115093f7c3790de8f2b927d593
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-01-27 15:11:22 +0100

    yunluo: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: 22081283C Script version: 3.0.0

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_yunluo-user
+add_lunch_combo omni_yunluo-userdebug
+add_lunch_combo omni_yunluo-eng
