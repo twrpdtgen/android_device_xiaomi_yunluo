b250d2efa6f1a5129f67c61b1631885832afb7e4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-02-25 10:18:10 +0100

    yunluo: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: 22081283G Script version: 3.0.0

---
+++ omni_yunluo.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from yunluo device
+$(call inherit-product, device/xiaomi/yunluo/device.mk)
+
+PRODUCT_DEVICE := yunluo
+PRODUCT_NAME := omni_yunluo
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := 22081283G
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="yunluo-user 12 SP1A.210812.016 V816.0.1.0.ULYMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/yunluo_global/yunluo:12/SP1A.210812.016/V816.0.1.0.ULYMIXM:user/release-keys
